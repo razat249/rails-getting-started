9ff1c226ab1505f0d29b5a0e3814d13d836b7bd2
Rajat Patwa <razat249@gmail.com>
2017-09-10 15:16:08 +0530

    Add edit method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,4 @@
 class ArticlesController < ApplicationController
-  def new
-  end
 
   def index
     @articles = Article.all
@@ -10,11 +8,32 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def new
+    @article = Article.new
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
   
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+  
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
   
   private
